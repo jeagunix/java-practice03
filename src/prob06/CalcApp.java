186c8ded0bbd2b3238cc8d77a1cda7a4e1f00eae
JeaGunix <dlwprn1541@gmail.com>
2019-08-11 18:22:45 +0900

    Update number6 problem.
    practice Java and grow my programming's level.

--- src/prob06/CalcApp.java
+++ src/prob06/CalcApp.java
@@ -2,36 +2,74 @@ package prob06;
 
 import java.util.Scanner;
 
+//제한조건 수
 public class CalcApp {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
-		while( true ) {
 
-			System.out.print( ">> " );
+		while (true) {
+
+			System.out.print(">> ");
 			String expression = scanner.nextLine();
-			
-			if( "quit".equals( expression ) ) {
+
+			if ("quit".equals(expression)) {
 				break;
 			}
-			
-			String[] tokens = expression.split( " " );
-			
-			if( tokens.length != 3 ) {
-				System.out.println( ">> 알 수 없는 식입니다.");
+
+			String[] tokens = expression.split(" ");
+
+			if (tokens.length != 3) {
+				System.out.println(">> 알 수 없는 식입니다.");
 				continue;
 			}
-			
 
-			
-			/*  코드를 완성 합니다 */
-			
-			
-			
-			
+			int lvalue = Integer.parseInt(tokens[0]);
+			int rvalue = Integer.parseInt(tokens[2]);
+
+			switch (tokens[1]) {
+			case "+": {
+				Add add = new Add();
+				add.setValue(lvalue, rvalue);
+
+				int result = add.calculate();
+				System.out.println(">>" + result);
+				break;
+			}
+			case "-": {
+				Sub sub = new Sub();
+				sub.setValue(lvalue, rvalue);
+
+				int result = sub.calculate();
+				System.out.println(">>" + result);
+				break;
+			}
+			case "/": {
+				Div div = new Div();
+				div.setValue(lvalue, rvalue);
+
+				int result = div.calculate();
+				System.out.println(">>" + result);
+				break;
+			}
+			case "*": {
+				Mul mul = new Mul();
+				mul.setValue(lvalue, rvalue);
+
+				int result = mul.calculate();
+				System.out.println(">>" + result);
+				break;
+			}
+			default: {
+				System.out.println(">>지원하지 않는 연산입니다.");
+				continue;
+			}
+			}
+
+			/* 코드를 완성 합니다 */
+
 		}
-		
+
 		scanner.close();
 
 	}
